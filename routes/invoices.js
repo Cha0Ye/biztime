5a5da97b73db49754d08c79f8c63d4b845fc0508
SaraZ <saraaban@gmail.com>
2019-03-07 12:26:02 -0800

    start GET/invoices/id

--- routes/invoices.js
+++ routes/invoices.js
@@ -20,5 +20,23 @@ router.get("/invoices", async function(req, res, next) {
     }
 });
 
+/** GET /invoices/:code get a single company */
+
+router.get("/invoices/:code", async function(req, res, next) {
+    try{
+        let code = req.params.code;
+        const result = await db.query(
+            `SELECT id, amt, paid, add_date, paid_date FROM invoices WHERE code=$1`, [code]);
+            // console.log(result.rows[0]);
+            if (result.rows[0]) {
+                return res.json({invoices: result.rows[0]});
+            }
+        throw new ExpressError("Company code does not exist", 404);
+        }
+    catch(err){
+        return next(err);
+    }
+});
+
 
 module.exports=router;
