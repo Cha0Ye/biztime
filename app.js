73b616bd9aca955e2fbf790020ffb0776fbf84d8
SaraZ <saraaban@gmail.com>
2019-03-07 11:14:17 -0800

    add: show single company

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const app = express();
 const ExpressError = require("./expressError")
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(companiesRoute);
 
 /** 404 handler */
--- routes/companies.js
+++ routes/companies.js
@@ -20,6 +20,23 @@ router.get("/companies", async function(req, res, next) {
     }
 });
 
+router.get("/companies/:code", async function(req, res, next) {
+    try{
+        let code = req.params.code;
+        const result = await db.query(
+            `SELECT code, name, description FROM companies WHERE code=$1`, [code]);
+            console.log(result.rows[0]);
+            if (result.rows[0]) {
+                return res.json({companies: result.rows[0]});
+            }
+        throw new ExpressError("Company code does not exist", 404);
+        }
+    catch(err){
+        return next(err);
+    }
+});
+
+
 // /companies** DELETE /users/[id]: delete user, return status */
 
 // router.delete("/companies/:id", function(req, res) {
