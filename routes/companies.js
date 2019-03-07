ec090d373b7324b642b2ce124074218071b02bbe
SaraZ <saraaban@gmail.com>
2019-03-07 11:54:57 -0800

    add: delete

--- routes/companies.js
+++ routes/companies.js
@@ -6,7 +6,7 @@ const ExpressError = require("../expressError")
 
 const db =  require('../db');
 
-/** GET /users: get list of users */
+/** GET /companies: get list of all companies */
 
 router.get("/companies", async function(req, res, next) {
     try{
@@ -19,6 +19,8 @@ router.get("/companies", async function(req, res, next) {
         return next(err);
     }
 });
+
+/** GET /companies/:code get a single company */
 
 router.get("/companies/:code", async function(req, res, next) {
     try{
@@ -36,6 +38,8 @@ router.get("/companies/:code", async function(req, res, next) {
     }
 });
 
+/** POST /companies: create a new company */
+
 router.post("/companies", async function(req, res, next) {
     try{
         const {code, name, description} = req.body;
@@ -52,15 +56,48 @@ router.post("/companies", async function(req, res, next) {
     }
 });
 
+/** PUT /companies/:code: update an existing company */
 
+router.put("/companies/:code", async function(req, res, next) {
+    try{
+        let code = req.params.code;
+        const {name, description} = req.body;
 
-// /companies** DELETE /users/[id]: delete user, return status */
+        const result = await db.query(
+            `SELECT code, name, description FROM companies WHERE code=$1`, [code]);
+            // console.log(result.rows[0]);
+            if (result.rows[0]) {
+                const result = await db.query(
+                    `UPDATE companies 
+                    SET name=$1, description=$2
+                    WHERE code=$3
+                    RETURNING code, name, description`, 
+                    [name, description, code]
+                );
+                return res.json(result.rows[0]);
+            }
+            throw new ExpressError("Company code does not exist", 404);
+    }
+    catch(err){
+        return next(err);
+    }
+});
 
-// router.delete("/companies/:id", function(req, res) {
-//   const idx = users.findIndex(u => u.id === +req.params.id);
-//   users.splice(idx, 1);
-//   return res.json({ message: "Deleted" });
-// });
+/** DELETE /companies/:code: update an existing company */
+
+router.delete("/companies/:code", async function(req, res, next) {
+    let code = req.params.code;
+    try {
+        const result = await db.query(
+            "DELETE FROM companies WHERE code = $1",
+            [code]
+        );
+        return res.json({message: "Deleted"});
+    }
+    catch (err) {
+        return next(err);
+    }
+    });
 
 
 module.exports = router;
